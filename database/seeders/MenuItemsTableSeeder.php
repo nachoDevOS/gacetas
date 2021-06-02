ef3cb25f917d2464d63c242f288aa5a84d70f2ab
Buddy <buddy.m091@gmail.com>
2021-06-02 15:20:25 -0400

    First commit

---
+++ database/seeders/MenuItemsTableSeeder.php
@@ -0,0 +1,202 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class MenuItemsTableSeeder extends Seeder
+{
+
+    /**
+     * Auto generated seed file
+     *
+     * @return void
+     */
+    public function run()
+    {
+        
+
+        \DB::table('menu_items')->delete();
+        
+        \DB::table('menu_items')->insert(array (
+            0 => 
+            array (
+                'id' => 1,
+                'menu_id' => 1,
+                'title' => 'Inicio',
+                'url' => '',
+                'target' => '_self',
+                'icon_class' => 'voyager-home',
+                'color' => '#000000',
+                'parent_id' => NULL,
+                'order' => 1,
+                'created_at' => '2021-06-02 17:55:32',
+                'updated_at' => '2021-06-02 14:51:15',
+                'route' => 'voyager.profile',
+                'parameters' => 'null',
+            ),
+            1 => 
+            array (
+                'id' => 2,
+                'menu_id' => 1,
+                'title' => 'Media',
+                'url' => '',
+                'target' => '_self',
+                'icon_class' => 'voyager-images',
+                'color' => NULL,
+                'parent_id' => 5,
+                'order' => 3,
+                'created_at' => '2021-06-02 17:55:32',
+                'updated_at' => '2021-06-02 14:07:22',
+                'route' => 'voyager.media.index',
+                'parameters' => NULL,
+            ),
+            2 => 
+            array (
+                'id' => 3,
+                'menu_id' => 1,
+                'title' => 'Users',
+                'url' => '',
+                'target' => '_self',
+                'icon_class' => 'voyager-person',
+                'color' => NULL,
+                'parent_id' => 11,
+                'order' => 1,
+                'created_at' => '2021-06-02 17:55:32',
+                'updated_at' => '2021-06-02 14:08:02',
+                'route' => 'voyager.users.index',
+                'parameters' => NULL,
+            ),
+            3 => 
+            array (
+                'id' => 4,
+                'menu_id' => 1,
+                'title' => 'Roles',
+                'url' => '',
+                'target' => '_self',
+                'icon_class' => 'voyager-lock',
+                'color' => NULL,
+                'parent_id' => 11,
+                'order' => 2,
+                'created_at' => '2021-06-02 17:55:32',
+                'updated_at' => '2021-06-02 14:08:05',
+                'route' => 'voyager.roles.index',
+                'parameters' => NULL,
+            ),
+            4 => 
+            array (
+                'id' => 5,
+                'menu_id' => 1,
+                'title' => 'Herramientas',
+                'url' => '',
+                'target' => '_self',
+                'icon_class' => 'voyager-tools',
+                'color' => '#000000',
+                'parent_id' => NULL,
+                'order' => 3,
+                'created_at' => '2021-06-02 17:55:32',
+                'updated_at' => '2021-06-02 14:08:05',
+                'route' => NULL,
+                'parameters' => '',
+            ),
+            5 => 
+            array (
+                'id' => 6,
+                'menu_id' => 1,
+                'title' => 'Menu Builder',
+                'url' => '',
+                'target' => '_self',
+                'icon_class' => 'voyager-list',
+                'color' => NULL,
+                'parent_id' => 5,
+                'order' => 1,
+                'created_at' => '2021-06-02 17:55:33',
+                'updated_at' => '2021-06-02 14:07:22',
+                'route' => 'voyager.menus.index',
+                'parameters' => NULL,
+            ),
+            6 => 
+            array (
+                'id' => 7,
+                'menu_id' => 1,
+                'title' => 'Database',
+                'url' => '',
+                'target' => '_self',
+                'icon_class' => 'voyager-data',
+                'color' => NULL,
+                'parent_id' => 5,
+                'order' => 2,
+                'created_at' => '2021-06-02 17:55:33',
+                'updated_at' => '2021-06-02 14:07:22',
+                'route' => 'voyager.database.index',
+                'parameters' => NULL,
+            ),
+            7 => 
+            array (
+                'id' => 8,
+                'menu_id' => 1,
+                'title' => 'Compass',
+                'url' => '',
+                'target' => '_self',
+                'icon_class' => 'voyager-compass',
+                'color' => NULL,
+                'parent_id' => 5,
+                'order' => 4,
+                'created_at' => '2021-06-02 17:55:33',
+                'updated_at' => '2021-06-02 14:07:22',
+                'route' => 'voyager.compass.index',
+                'parameters' => NULL,
+            ),
+            8 => 
+            array (
+                'id' => 9,
+                'menu_id' => 1,
+                'title' => 'BREAD',
+                'url' => '',
+                'target' => '_self',
+                'icon_class' => 'voyager-bread',
+                'color' => NULL,
+                'parent_id' => 5,
+                'order' => 5,
+                'created_at' => '2021-06-02 17:55:33',
+                'updated_at' => '2021-06-02 14:07:23',
+                'route' => 'voyager.bread.index',
+                'parameters' => NULL,
+            ),
+            9 => 
+            array (
+                'id' => 10,
+                'menu_id' => 1,
+                'title' => 'Settings',
+                'url' => '',
+                'target' => '_self',
+                'icon_class' => 'voyager-settings',
+                'color' => NULL,
+                'parent_id' => 5,
+                'order' => 6,
+                'created_at' => '2021-06-02 17:55:33',
+                'updated_at' => '2021-06-02 14:07:25',
+                'route' => 'voyager.settings.index',
+                'parameters' => NULL,
+            ),
+            10 => 
+            array (
+                'id' => 11,
+                'menu_id' => 1,
+                'title' => 'Seguridad',
+                'url' => '',
+                'target' => '_self',
+                'icon_class' => 'voyager-lock',
+                'color' => '#000000',
+                'parent_id' => NULL,
+                'order' => 2,
+                'created_at' => '2021-06-02 14:07:53',
+                'updated_at' => '2021-06-02 14:08:05',
+                'route' => NULL,
+                'parameters' => '',
+            ),
+        ));
+        
+        
+    }
+}
---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+
+    /**
+     * Auto generated seed file
+     *
+     * @return void
+     */
+    public function run()
+    {
+        
+
+        \DB::table('users')->delete();
+        
+        \DB::table('users')->insert(array (
+            0 => 
+            array (
+                'id' => 1,
+                'role_id' => 1,
+                'name' => 'Admin',
+                'email' => 'admin@admin.com',
+                'avatar' => 'users/default.png',
+                'email_verified_at' => NULL,
+                'password' => '$2y$10$piw47ZDCJq3ieZXMVap/2eo1j71yvn4g2LNCxJu7b/7JEgq/8Q4oG',
+                'remember_token' => NULL,
+                'settings' => NULL,
+                'created_at' => '2021-06-01 21:05:11',
+                'updated_at' => '2021-06-01 21:05:11',
+            ),
+        ));
+        
+        
+    }
+}
